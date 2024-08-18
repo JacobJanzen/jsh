17396713bff0ac09ec8bde911c53f61a991b1aac
Jacob Janzen <jacob.a.s.janzen@gmail.com>
2024-08-18 18:31:19 -0500

    refactor lexer

--- src/lexer.c
+++ src/lexer.c
@@ -6,14 +6,14 @@
 
 #define DEFAULT_CAPACITY 16
 
-enum token_delimeter {
+enum delimiter {
     DEL_DEFAULT,
-    DEL_DQUOTE,
+    DEL_PAR,
+    DEL_DPAR,
+    DEL_BRACE,
     DEL_QUOTE,
-    DEL_RPAR,
-    DEL_DRPAR,
-    DEL_RBRACE,
-    DEL_BACKQUOTE,
+    DEL_DQUOTE,
+    DEL_BQUOTE,
 };
 
 struct string {
@@ -22,13 +22,6 @@ struct string {
     size_t capacity;
 };
 
-int saved_char = -1;
-int can_be_io_number;
-int can_be_name;
-int can_be_assignment;
-int can_be_grammatical_assignment = 1;
-int can_be_grammatical_name = 0;
-int first_char;
 FILE *file;
 
 static void string_append(struct string *s, char c)
@@ -42,195 +35,162 @@ static void string_append(struct string *s, char c)
 
 static int is_blank(char c)
 {
-    return c == ' ' || c == '\t' || c == '\f' || c == '\v';
+    return c == ' ' || c == '\t' || c == '\f' || c == '\v' || c == '\r';
 }
 
-static void recognize_token(struct string *str, enum token_delimeter del)
+static void delimit_token(struct string *str, enum delimiter del)
 {
-    int done = 0;
-    while (!done) {
-        char c;
-        if (saved_char == -1)
+    while (1) {
+        int c = getc(file);
+
+        switch (c) {
+        case EOF: /* exit on EOF */
+            return;
+        case '\\': /* handle escape character */
             c = getc(file);
-        else
-            c = saved_char;
-        saved_char = -1;
-
-        if (c == 0 || c == EOF) {
-            done = 1;
-        } else if (del == DEL_QUOTE) {
-            string_append(str, c);
-            if (c == '\'')
-                done = 1;
-        } else {
-            if (c == '\\') {
-                c = getc(file);
-
-                if (c == 0 || c == EOF) {
-                    string_append(str, '\\');
-                    done = 1;
-                } else if (c != '\n') {
-                    /* don't save escaped newlines */
-                    string_append(str, '\\');
-                    string_append(str, c);
-                }
-                can_be_name = 0;
-                can_be_io_number = 0;
-            } else if (c == '$') {
+            switch (c) {
+            case EOF:
+                return;
+            case '\n':
+                break;
+            default:
                 string_append(str, c);
-                c = getc(file);
-
-                if (c == 0 || c == EOF) {
-                    done = 1;
-                } else if (c == '{') {
-                    /* identify parameter expansion */
-                    string_append(str, c);
-                    recognize_token(str, DEL_RBRACE);
-                } else if (c == '(') {
-                    string_append(str, c);
-                    c = getc(file);
-
-                    if (c == 0 || c == EOF) {
-                        done = 1;
-                    } else if (c == '(') {
-                        /* identify arithmetic expansion */
-                        string_append(str, c);
-                        recognize_token(str, DEL_DRPAR);
-
-                    } else {
-                        /* identify command substitution */
-                        saved_char = c;
-                        recognize_token(str, DEL_RPAR);
-                    }
-                } else {
-                    saved_char = c;
-                }
-                can_be_name = 0;
-                can_be_io_number = 0;
-            } else if (c == '`') {
-                /* identify command substitution */
-                string_append(str, c);
-                if (del == DEL_BACKQUOTE) {
-                    done = 1;
-                } else {
-                    recognize_token(str, DEL_BACKQUOTE);
-                }
-                can_be_name = 0;
-                can_be_io_number = 0;
-            } else if (c == '}' && del == DEL_RBRACE) {
-                /* close parameter expansion */
-                string_append(str, c);
-                done = 1;
-            } else if (c == ')' && (del == DEL_RPAR || del == DEL_DRPAR)) {
-                /* close command substitution or drop into command substitution
-                 * mode if in arithmetic expansion */
-                string_append(str, c);
-                if (del == DEL_DRPAR) {
-                    recognize_token(str, DEL_RPAR);
-                }
-                done = 1;
-            } else if (c == '\'' && del != DEL_DQUOTE && del != DEL_DRPAR) {
-                /* identify single quote */
-                string_append(str, c);
-                recognize_token(str, DEL_QUOTE);
-                can_be_name = 0;
-                can_be_io_number = 0;
-            } else if (c == '\"' && del != DEL_RBRACE) {
-                /* identify double quote */
-                string_append(str, c);
-                if (del == DEL_DQUOTE) {
-                    done = 1;
-                } else {
-                    recognize_token(str, DEL_DQUOTE);
-                }
-                can_be_name = 0;
-                can_be_io_number = 0;
-            } else if (del == DEL_DEFAULT &&
-                       (c == '&' || c == '|' || c == ';' || c == '<' ||
-                        c == '>' || c == '(' || c == ')' || c == '\n' ||
-                        is_blank(c))) {
-                /* identify end of word by operator or blank */
-                string_append(str, 0);
-                saved_char = c;
-                done = 1;
-            } else if (c == '#' && del != DEL_DQUOTE && del != DEL_DRPAR) {
-                /* identify comment */
-                c = getc(file);
-                while (c != '\n' && c != 0)
-                    c = getc(file);
-                saved_char = c;
-            } else {
-                /* add anything else to the word */
-                string_append(str, c);
-
-                if ((c <= 'a' || c >= 'z') && (c <= 'A' || c >= 'Z') &&
-                    (c <= '0' || c >= '9') && c != '_') {
-                    can_be_name = 0;
-                    if (c == '=' && !first_char) {
-                        can_be_assignment = 1;
-                    }
-                }
-                if (c <= '0' || c >= '9')
-                    can_be_io_number = 0;
             }
+        case '\'': /* enter/exit single quote */
+            string_append(str, c);
+            if (del == DEL_QUOTE)
+                return;
+            if (del != DEL_DQUOTE)
+                delimit_token(str, DEL_QUOTE);
+            break;
+        case '\"': /* enter/exit double quote */
+            string_append(str, c);
+            if (del == DEL_DQUOTE)
+                return;
+            if (del != DEL_QUOTE)
+                delimit_token(str, DEL_DQUOTE);
+            break;
+        case '`': /* enter/exit backquote */
+            string_append(str, c);
+            if (del == DEL_BQUOTE)
+                return;
+            if (del != DEL_QUOTE)
+                delimit_token(str, DEL_BQUOTE);
+            break;
+        case '$':
+            string_append(str, c);
+            c = getc(file);
+            switch (c) {
+            case '{': /* enter paramter expansion */
+                string_append(str, c);
+                delimit_token(str, DEL_BRACE);
+                break;
+            case '(':
+                string_append(str, c);
+                c = getc(file);
+                if (c == '(') { /* enter arithmetic expansion */
+                    string_append(str, c);
+                    delimit_token(str, DEL_DPAR);
+                } else { /* enter command substitution */
+                    ungetc(c, file);
+                    delimit_token(str, DEL_PAR);
+                }
+                break;
+            default:
+                ungetc(c, file);
+            }
+            break;
+        case ')':
+            if (del == DEL_DEFAULT) /* special character; exit by default */
+                return;
+            string_append(str, c);
+            if (del == DEL_DPAR) { /* exit arithmetic expansion */
+                delimit_token(str, DEL_PAR);
+                return;
+            }
+            if (del == DEL_PAR) /* exit command substitution */
+                return;
+            break;
+        case '}': /* exit parameter expansion */
+            string_append(str, c);
+            if (del == DEL_BRACE)
+                return;
+            break;
+        case '#': /* enter comment */
+            if (del == DEL_DEFAULT || del == DEL_PAR) {
+                while (c != '\n' && c != EOF)
+                    c = getc(file);
+                ungetc(c, file);
+            } else
+                string_append(str, c);
+            break;
+        case '&':  /* fallthrough */
+        case '|':  /* fallthrough */
+        case ';':  /* fallthrough */
+        case '<':  /* fallthrough */
+        case '>':  /* fallthrough */
+        case ' ':  /* fallthrough */
+        case '\t': /* fallthrough */
+        case '\f': /* fallthrough */
+        case '\v': /* fallthrough */
+        case '\r': /* fallthrough */
+        case '\n':
+            if (del == DEL_DEFAULT) { /* exit default environment */
+                ungetc(c, file);
+                return;
+            }
+            string_append(str, c);
+            break;
+        default: /* don't do anything special with other characters */
+            string_append(str, c);
         }
-        first_char = 0;
     }
 }
+
+static int recognize_token(struct string *str) { return WORD; }
 
 int yylex(void)
 {
-    int c;
-    if (saved_char == -1)
-        c = getc(file);
-    else
-        c = saved_char;
-    saved_char = -1;
+    int c = getc(file);
 
     /* skip whitespace */
-    while (is_blank(c) && c != 0)
+    while (is_blank(c))
         c = getc(file);
 
-    /* skip comment */
-    if (c == '#') {
-        while (c != '\n' && c != 0)
-            c = getc(file);
-    }
-
+    /* determine token */
     switch (c) {
-    case 0: /* fallthrough */
-    case EOF:
-        printf("EOF ");
-        return YYEOF;
+    case '(': /* fallthrough */
+    case ')':
+        printf("%c", c);
+        return c;
     case '\n':
         printf("\n");
-        can_be_grammatical_assignment = 1;
         return NEWLINE;
     case '&':
         c = getc(file);
         if (c == '&') {
             printf("AND_IF ");
-            return AND_IF;
+            return AND_IF; /* && */
         }
-        saved_char = c;
+        ungetc(c, file);
         printf("& ");
         return '&';
     case '|':
         c = getc(file);
         if (c == '|') {
             printf("OR_IF ");
-            return OR_IF;
+            return OR_IF; /* || */
         }
-        saved_char = c;
+        ungetc(c, file);
         printf("| ");
         return '|';
     case ';':
         c = getc(file);
         if (c == ';') {
             printf("DSEMI ");
-            return DSEMI;
+            return DSEMI; /* ;; */
         }
-        saved_char = c;
+        ungetc(c, file);
         printf("; ");
         return ';';
     case '<':
@@ -239,182 +199,71 @@ int yylex(void)
             c = getc(file);
             if (c == '-') {
                 printf("DLESSDASH ");
-                return DLESSDASH;
+                return DLESSDASH; /* <<- */
             }
-            saved_char = c;
+            ungetc(c, file);
             printf("DLESS ");
-            return DLESS;
+            return DLESS; /* << */
         }
         if (c == '&') {
             printf("LESSAND ");
-            return LESSAND;
+            return LESSAND; /* <& */
         }
         if (c == '>') {
             printf("LESSGREAT ");
-            return LESSGREAT;
+            return LESSGREAT; /* <> */
         }
-        saved_char = c;
         printf("< ");
         return '<';
     case '>':
         c = getc(file);
         if (c == '>') {
             printf("DGREAT ");
-            return DGREAT;
+            return DGREAT; /* >> */
         }
         if (c == '&') {
             printf("GREATAND ");
-            return GREATAND;
+            return GREATAND; /* >& */
         }
         if (c == '|') {
             printf("CLOBBER ");
-            return CLOBBER;
+            return CLOBBER; /* >| */
         }
-        saved_char = c;
+        ungetc(c, file);
         printf("> ");
         return '>';
-    case '(': /* fallthrough */
-    case ')':
-        printf("%c ", c);
-        return c;
-    default: {
-        saved_char = c;
-        if ((c <= 'a' || c >= 'z') && (c <= 'A' || c >= 'Z') && c != '_')
-            can_be_name = 0;
-        else
-            can_be_name = 1;
-        can_be_assignment = 0;
-        if (c >= '0' && c <= '9')
-            can_be_io_number = 1;
-        else
-            can_be_io_number = 0;
-
-        struct string *str = malloc(sizeof(struct string));
-        str->capacity = DEFAULT_CAPACITY;
-        str->size = 0;
-        str->container = malloc(DEFAULT_CAPACITY);
-        first_char = 1;
-        recognize_token(str, DEL_DEFAULT);
-
-        yylval = str->container;
-        if (strcmp(str->container, "!") == 0) {
-            can_be_grammatical_assignment = 0;
-            printf("Bang ");
-            return Bang;
-        }
-        if (strcmp(str->container, "{") == 0) {
-            can_be_grammatical_assignment = 0;
-            printf("Lbrace ");
-            return Lbrace;
-        }
-        if (strcmp(str->container, "}") == 0) {
-            can_be_grammatical_assignment = 0;
-            printf("Rbrace ");
-            return Rbrace;
-        }
-        if (strcmp(str->container, "case") == 0) {
-            can_be_grammatical_assignment = 0;
-            printf("Case ");
-            return Case;
-        }
-        if (strcmp(str->container, "do") == 0) {
-            can_be_grammatical_assignment = 0;
-            printf("Do ");
-            return Do;
-        }
-        if (strcmp(str->container, "done") == 0) {
-            can_be_grammatical_assignment = 0;
-            printf("Done ");
-            return Done;
-        }
-        if (strcmp(str->container, "elif") == 0) {
-            can_be_grammatical_assignment = 0;
-            printf("Elif ");
-            return Elif;
-        }
-        if (strcmp(str->container, "else") == 0) {
-            can_be_grammatical_assignment = 0;
-            printf("Else ");
-            return Else;
-        }
-        if (strcmp(str->container, "esac") == 0) {
-            can_be_grammatical_assignment = 0;
-            printf("Esac ");
-            return Esac;
-        }
-        if (strcmp(str->container, "fi") == 0) {
-            can_be_grammatical_assignment = 0;
-            printf("Fi ");
-            return Fi;
-        }
-        if (strcmp(str->container, "for") == 0) {
-            can_be_grammatical_assignment = 0;
-            can_be_grammatical_name = 1;
-            printf("For ");
-            return For;
-        }
-        if (strcmp(str->container, "if") == 0) {
-            can_be_grammatical_assignment = 0;
-            printf("If ");
-            return If;
-        }
-        if (strcmp(str->container, "in") == 0) {
-            can_be_grammatical_assignment = 0;
-            printf("In ");
-            return In;
-        }
-        if (strcmp(str->container, "then") == 0) {
-            can_be_grammatical_assignment = 0;
-            printf("Then ");
-            return Then;
-        }
-        if (strcmp(str->container, "until") == 0) {
-            can_be_grammatical_assignment = 0;
-            printf("Until ");
-            return Until;
-        }
-        if (strcmp(str->container, "while") == 0) {
-            can_be_grammatical_assignment = 0;
-            printf("While ");
-            return While;
-        }
-        if (can_be_name) {
-            if (saved_char == -1)
-                c = getc(file);
-            else
-                c = saved_char;
-            while (is_blank(c) && c != 0)
-                c = getc(file);
-            saved_char = c;
-            if (can_be_grammatical_name || c == '(') {
-                printf("NAME(%s) ", str->container);
-                return NAME;
-            }
-        }
-        if (can_be_assignment && can_be_grammatical_assignment) {
-            printf("ASSIGNMENT_WORD(%s) ", str->container);
-            return ASSIGNMENT_WORD;
-        }
-        if (can_be_io_number) {
-            can_be_grammatical_assignment = 0;
-            if (saved_char == -1)
-                c = getc(file);
-            else
-                c = saved_char;
-            while (is_blank(c) && c != 0)
-                c = getc(file);
-            saved_char = c;
-            if (c == '<' || c == '>') {
-                printf("IO_NUMBER(%s) ", str->container);
-                return IO_NUMBER;
-            }
+    case '#':
+        while (c != '\n' && c != EOF)
+            c = getc(file);
+        if (c == '\n')
+            return NEWLINE;
+        if (c == EOF)
+            return YYEOF;
+    case EOF:
+        printf("DONE ");
+        return YYEOF;
+    default: /* recognize a WORD, NAME, IO_NUMBER, or ASSIGNMENT_WORD */
+    {
+        /* create a string to store the token in */
+        struct string str;
+        str.capacity = DEFAULT_CAPACITY;
+        str.size = 0;
+        str.container = malloc(DEFAULT_CAPACITY);
+        if (!str.container) {
+            perror("malloc");
+            return ERROR;
         }
 
-        can_be_grammatical_assignment = 0;
-        printf("WORD(%s) ", str->container);
-        return WORD;
+        ungetc(c, file);
+        delimit_token(&str, DEL_DEFAULT);
+        string_append(&str, 0);
+        printf("WORD(%s) ", str.container);
+        free(str.container);
+        return recognize_token(&str);
     }
     }
+
+    return 0;
 }
 
 void yyerror(char const *s) { fprintf(stderr, "%s\n", s); }
